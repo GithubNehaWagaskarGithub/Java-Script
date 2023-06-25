8bfc41f8c794b6b9635fba51dd219f4b82a0375d
GithubNehaWagaskarGithub <133528554+GithubNehaWagaskarGithub@users.noreply.github.com>
2023-06-25 19:48:12 +0530

    Add files via upload

---
+++ DifferentOutputMethods/index.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+alert("Alert")
+confirm("I am confirm")
+document.writeln("My place is mysore")
+document.write("My name is sudesh")
+console.error("Error 404")
+console.warn("Warning !!!😊")
