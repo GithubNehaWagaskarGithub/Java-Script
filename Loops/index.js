7bf66c3fc28cdbc3122a49408ccbf79ef42f1776
GithubNehaWagaskarGithub <133528554+GithubNehaWagaskarGithub@users.noreply.github.com>
2023-06-25 19:51:46 +0530

    Add files via upload

---
+++ Loops/index.js
@@ -0,0 +1,19 @@
+let i=1;
+let n=10;
+/*while (i<=n) {
+    console.log(i);
+    i++;
+}*/
+
+/*do {
+    console.log(i);
+    i++;
+} while (i<=n);*/
+
+for (let i=1
+    
+    
+    ;i<5;i++) {
+    console.log(i);
+    
+}
