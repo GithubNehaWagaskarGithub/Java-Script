ceae3c048f9483fd5b74297d23e096e450fd5651
GithubNehaWagaskarGithub <133528554+GithubNehaWagaskarGithub@users.noreply.github.com>
2023-06-25 19:33:56 +0530

    Add files via upload

---
+++ Condition/index.js
@@ -0,0 +1,47 @@
+/*let age=18;
+if(age>=18){
+    console.log("Eligible for Voting");
+}*/
+
+/*let age =17;
+if(age>=18)
+{
+    console.log("Eligible for voting");
+}
+else{
+    console.log("Not Eligible");
+}*/
+
+/*let age=18
+if(age==5)
+{
+    console.log("Join lkg");
+}
+else if(age==6)
+{
+    console.log("Join ukg");
+}
+else if(age==7)
+{
+    console.log("Join 1std");
+}
+else
+{
+    console.log("Enter Correct age!!");
+}*/
+
+let time=6;
+switch (time) {
+    case 6:
+        console.log("Good Morning");
+        break;
+
+    case 12:
+        console.log("Good Afternoon");
+        break;
+        
+    default:
+        console.log("Enter correct time");
+        break;
+}
+
